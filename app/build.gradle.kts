49be80022d01f810f7a8a104101b04f9081389fb
ElVitalya <kuraev.vf@gmail.com>
2025-12-05 16:44:50 +0500

    version 0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.kvf.callblocker"
         minSdk = 35
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
